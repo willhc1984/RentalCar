75047f0373c45591d10b70f0302ade5975f4bfcb
will henrique <willhc1984@gmail.com>
2022-06-08 11:17:34 -0300

    Criação camada service

--- src/model/entities/CarRental.java
+++ src/model/entities/CarRental.java
@@ -1,5 +1,58 @@
 package model.entities;
 
+import java.util.Date;
+
 public class CarRental {
+	
+	private Date start;
+	private Date finish;
+	
+	private Invoice invoice;
+	private Vehicle vehicle;
+	
+	public CarRental() {
+		
+	}
+	
+	public CarRental(Date start, Date finish, Vehicle vehicle) {
+		super();
+		this.start = start;
+		this.finish = finish;
+		this.vehicle = vehicle;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getFinish() {
+		return finish;
+	}
+
+	public void setFinish(Date finish) {
+		this.finish = finish;
+	}
+
+	public Invoice getInvoice() {
+		return invoice;
+	}
+
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	
 
 }
--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -1,5 +1,66 @@
 package model.services;
 
+import model.entities.CarRental;
+import model.entities.Invoice;
+
 public class RentalService {
+	
+	private Double pricePerHour;
+	private Double pricePerDay;
+	private BrazilTaxService taxService;
+	
+	public RentalService() {	
+	}
+	
+	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		
+		double hours = (double) (t2-t1) / 1000 / 60 / 60;
+		
+		double basicPayment;
+		if(hours <= 12.0) {
+			basicPayment = pricePerHour * Math.ceil(hours);
+		}
+		else {
+			basicPayment = pricePerDay * Math.ceil(hours / 24);
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+
+	public Double getPricePerHour() {
+		return pricePerHour;
+	}
+
+	public void setPricePerHour(Double pricePerHour) {
+		this.pricePerHour = pricePerHour;
+	}
+
+	public Double getPricePerDay() {
+		return pricePerDay;
+	}
+
+	public void setPricePerDay(Double pricePerDay) {
+		this.pricePerDay = pricePerDay;
+	}
+
+	public BrazilTaxService getTax() {
+		return taxService;
+	}
+
+	public void setTax(BrazilTaxService tax) {
+		this.taxService = tax;
+	}
+	
+	
+	
 
 }
